6de9fcfb81d89cf2363f19bb31de00538ab1d9b0
Mario <mqs@melix.org>
2017-05-03 18:51:08 +0200

    adding Church and Barendregt

--- testingLambdaCalculus.js
+++ testingLambdaCalculus.js
@@ -1,3 +1,22 @@
+//Church numerals
+var c_0 = function(f) {
+    return function(x) {
+        return x;
+    }
+};
+
+var c_1 = function(f) {
+    return function(x) {
+        return f(x);
+    }
+};
+
+var c_2 = function(f) {
+    return function(x) {
+        return f(f(x));
+    }
+};
+
 // this is the crochet operator [M,N](boolean)
 var IF = function(boolean) {
     return function(x) {
@@ -19,6 +38,13 @@ var _false = function(x) {
     }
 };
 
+var test_true = IF(_true)(1)(2);
+console.log(test_true);
+
+var test_false = IF(_false)(1)(2);
+console.log(test_false);
+
+//Barendregt natural numbers
 var zero = function(x) {
     return x;
 }
@@ -27,13 +53,6 @@ var one = function(z) {
     return IF(z)(_false)(zero);
 }
 
-var two = IF(_false)(one)
-
-console.log(one(123));
-
-var test_true = IF(_true)(1)(2);
-console.log(test_true);
-
-var test_false = IF(_false)(1)(2);
-console.log(test_false);
-
+var two = function(z) {
+    return IF(z)(_false)(one);
+}
