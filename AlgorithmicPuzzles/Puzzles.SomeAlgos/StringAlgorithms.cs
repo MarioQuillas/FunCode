c722acbc507f93a7f5c289c069714440425a5ddf
Mario <mqs@melix.org>
2017-05-04 20:39:12 +0200

    just adding some projects

---
+++ AlgorithmicPuzzles/Puzzles.SomeAlgos/StringAlgorithms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles.SomeAlgos
+{
+    class StringAlgorithms
+    {
+        public List<string> AllPermutations(string word)
+        {
+            //var toto = ne
+
+            return null;
+        }
+
+
+
+        public bool AreAnagrams(string word1, string word2)
+        {
+
+
+
+            return false;
+        }
+    }
+}
