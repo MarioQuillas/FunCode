ddd59ef30f9ba6398ae14af5c271f33cbbe76617
Mario <mqs@melix.org>
2017-05-09 18:10:17 +0200

    making the console a little more user friendly

--- AlgorithmicPuzzles/Puzzles.Console/Program.cs
+++ AlgorithmicPuzzles/Puzzles.Console/Program.cs
@@ -11,7 +11,11 @@ namespace Puzzles.Console
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("introduce a word to get all its permutations");
+
+
             var line = System.Console.ReadLine();
+            System.Console.WriteLine();
             var result = new StringAlgorithms().AllPermutations(line);
 
             foreach (var word in result)
@@ -20,8 +24,11 @@ namespace Puzzles.Console
             }
 
             System.Console.WriteLine();
-
-            foreach (var word in new StringAlgorithms().GetOnlyAnagrams(new List<string>() { "123", "451", "1021", "321", "aim", "mia", "154", "toto" }))
+            System.Console.WriteLine(
+                "getting all words that are anagrams from the list : \"123\", \"451\", \"1021\", \"321\", \"aim\", \"mia\", \"154\", \"toto\"");
+            System.Console.WriteLine();
+            foreach (var word in new StringAlgorithms().GetOnlyAnagrams(
+                new List<string>() {"123", "451", "1021", "321", "aim", "mia", "154", "toto"}))
             {
                 System.Console.WriteLine(word);
             }
