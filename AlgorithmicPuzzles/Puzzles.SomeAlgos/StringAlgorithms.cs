aeccfbf27294672573f5b88dd84d60ecfb78c5a3
Mario <mqs@melix.org>
2017-05-09 17:30:44 +0200

    refactoring all permutations to yield

--- AlgorithmicPuzzles/Puzzles.SomeAlgos/StringAlgorithms.cs
+++ AlgorithmicPuzzles/Puzzles.SomeAlgos/StringAlgorithms.cs
@@ -9,41 +9,40 @@ namespace Puzzles.SomeAlgos
 {
     public class StringAlgorithms
     {
-        public List<string> AllPermutations(string word)
+        public IEnumerable<string> AllPermutations(string word)
         {
             return this.AllPermutations(word.ToList());
         }
 
-        public List<string> AllPermutations(List<char> list)
+        public IEnumerable<string> AllPermutations(List<char> list)
         {
-            if (list.Count == 0) return new List<string>(){""};
-
-            var result = new List<string>();
-            var length = list.Count;
-
-            var pivotCharacter = list[0];
-            var pivotList = new List<char>();
-
-            for (var i = 1; i < length; ++i)
+            if (list.Count == 0) yield return "";
+            else
             {
-                pivotList.Add(list[i]);
+                var length = list.Count;
+
+                var pivotCharacter = list[0];
+                var pivotList = new List<char>();
+
+                for (var i = 1; i < length; ++i)
+                {
+                    pivotList.Add(list[i]);
+                }
+
+                var pivotPermutations = this.AllPermutations(pivotList);
+
+                for (var i = 0; i < length; ++i)
+                {
+                    foreach (var word in pivotPermutations)
+                    {
+                        yield return word.Insert(i, pivotCharacter.ToString());
+                    }
+                }
             }
-
-            var pivotPermutations = this.AllPermutations(pivotList);
-
-            for (var i = 0; i < length; ++i)
-            {
-                result.AddRange(pivotPermutations.Select(word => word.Insert(i, pivotCharacter.ToString())));
-            }
-
-            return result;
         }
 
         public bool AreAnagrams(string word1, string word2)
         {
-
-
-
             return false;
         }
     }
