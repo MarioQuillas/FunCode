97453a3eaf5162505d93d9015ba045ced2077055
Mario <mqs@melix.org>
2017-05-12 19:21:36 +0200

    starting f# version
    not yet working at all

--- AlgorithmicPuzzles/Puzzles.ScriptTests/Tutorial.fsx
+++ AlgorithmicPuzzles/Puzzles.ScriptTests/Tutorial.fsx
@@ -1,1 +1,9 @@
-﻿
+﻿let rnd = new System.Random()
+
+
+
+let rec allPermutations charList = 
+    match charList with
+    | [] -> ""
+    | head::tail -> 
+        let pivotPermutations = allPermutations(tail)
