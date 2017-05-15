fa5bf3355d49746dc0154bdb5b20f72637170c4e
Mario <mqs@melix.org>
2017-05-15 16:35:57 +0200

    finish f# version

--- AlgorithmicPuzzles/Puzzles.ScriptTests/Tutorial.fsx
+++ AlgorithmicPuzzles/Puzzles.ScriptTests/Tutorial.fsx
@@ -3,7 +3,21 @@
 
 
 let rec allPermutations charList = 
-    match charList with
-    | [] -> ""
-    | head::tail -> 
-        let pivotPermutations = allPermutations(tail)
+    seq {
+        match charList with
+        | [] -> yield ""
+        | head::tail -> 
+            let pivotPermutations = allPermutations(tail)
+            let length = charList.Length
+            for i in 0 .. length - 1 do
+                for word in pivotPermutations do
+                    yield word.Insert(i, head.ToString())
+    }
+
+let allWordPermutations (word:string) =
+    let t = word.ToCharArray() :> seq<char> 
+    allPermutations (List.ofSeq t)
+
+let printAllWordPermutations word =
+    for w in allWordPermutations(word) do
+        printfn "%s" w
